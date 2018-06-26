457295d46aca850df52ba0ea855083b2305dcf56
mengaldo <gianmarco.mengaldo@gmail.com>
2018-06-26 16:40:54 -0700

    minor.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include <math.h>
 
 
-const int Dim=3;
+const int Dim = 3;
 using namespace octree;
 
 int main(int argc, char *argv[])
@@ -22,35 +22,34 @@ int main(int argc, char *argv[])
     key.print_binary();
 
     LinearTree<Dim, double>::coordinate_t point((1));
-    point[0]=3;
-    point[1]=4; 
-    point[2]=5; 
+    point[0] = 3;
+    point[1] = 4;
+    point[2] = 5;
 
-    auto extent=point;
+    auto extent = point;
     LinearTree<Dim,double> lt(extent);
 
 
     //Initialze:
-    for(auto it=lt.begin_nodes();it!=lt.end_nodes();++it)
+    for(auto it = lt.begin_nodes(); it != lt.end_nodes(); ++it)
         it->data(0.0);
+    
     std::ofstream ofs0("points.txt");
-    for(auto it=lt.begin_nodes();it!=lt.end_nodes();++it)
-        ofs0<<it->real_coordinate()<<std::endl;
+    
+    for(auto it = lt.begin_nodes(); it != lt.end_nodes(); ++it)
+        ofs0 << it->real_coordinate() << std::endl;
 
 
     //Refine 3 times:
-    for(int i =0; i<4;++i)
+    for (int i = 0; i < 4; ++i)
     {
-        int count=0;
-        for(auto it=lt.begin_nodes();it!=lt.end_nodes();++it)
+        int count = 0;
+        for (auto it = lt.begin_nodes(); it != lt.end_nodes(); ++it)
         {
-            if(count==1)
+            if (count == 1)
             {
-                lt.refine(it, [&i](const LinearTree<Dim,double>::leaf_t& _t)
-                        {
-                        _t.data(static_cast<double>(i));
-                        }
-                        );
+                lt.refine(it, [&i](const LinearTree<Dim,double>::leaf_t& _t){
+                        _t.data(static_cast<double>(i));});
             }
             ++count;
         }
@@ -58,15 +57,15 @@ int main(int argc, char *argv[])
 
     //Coarsend the mesh again
     std::ofstream ofs1("points_refined.txt");
-    for(auto it=lt.begin_nodes();it!=lt.end_nodes();++it)
+    for (auto it = lt.begin_nodes(); it != lt.end_nodes(); ++it)
         ofs1<<it->real_coordinate()<<std::endl;
 
-    for(int i =0; i<4;++i)
+    for (int i = 0; i < 4; ++i)
     {
-        int count=0;
-        for(auto it=lt.begin_nodes();it!=lt.end_nodes();++it)
+        int count = 0;
+        for (auto it = lt.begin_nodes(); it != lt.end_nodes(); ++it)
         {
-            if(count==1)
+            if (count == 1)
             {
                 lt.coarsen(it);
             }
@@ -75,13 +74,8 @@ int main(int argc, char *argv[])
     }
 
     std::ofstream ofs2("points_coarsened.txt");
-    for(auto it=lt.begin_nodes();it!=lt.end_nodes();++it)
-        ofs2<<it->real_coordinate()<<std::endl;
-
-    
-
-
-
+    for (auto it = lt.begin_nodes(); it != lt.end_nodes(); ++it)
+        ofs2 << it->real_coordinate() << std::endl;
 
     return 0;
 }

409ff262678a64b86489de0fb90fda84e0a1d52c
Benedikt <benedikt.dorschner@lav.mavt.ethz.ch>
2018-07-10 22:19:48 -0700

    crtp

---
+++ utilities/crtp.hpp
@@ -0,0 +1,38 @@
+#ifndef INCLUDED_CRTP_HPP
+#define INCLUDED_CRTP_HPP
+
+namespace crtp
+{
+
+template <typename DerivedType, template<typename> class CrtpType>
+struct Crtp
+{
+    DerivedType& derived() { return static_cast<DerivedType&>(*this); }
+    DerivedType const& derived() const 
+    { 
+        return static_cast<DerivedType const&>(*this); 
+    }
+private:
+    Crtp(){}
+    friend CrtpType<DerivedType>;
+};
+
+/*Example Crtp
+template<class T>
+struct Base : Crtp<T,Base>
+{
+    void foo(){ this->derived()->foo(); }
+    int a =10;
+};
+struct Mixin : public Base<Mixin>
+{
+    void foo() {
+        std::cout<<"Mixing using a="
+        <<a<<std::endl; 
+    }
+};
+*/
+
+}
+
+#endif

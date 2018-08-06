47f2167146a750ddc886ced82815ffb2d414c737
YUKE <yuke1@YUKEdeMacBook-Pro.local>
2018-08-06 15:52:08 -0700

    some refinement of python part. with error vs dis plot / Plz use python3

--- lgf/lgf_table_gen/lgf_asym_error.py
+++ lgf/lgf_table_gen/lgf_asym_error.py
@@ -2,50 +2,42 @@ import scipy.special as special
 import scipy.integrate as integrate
 import numpy as np
 import math
+import random
 
-f = lambda n1, n2, n3,t : special.ive(n1,2*t) * special.ive(n2,2*t) *special.ive(n3,2*t)
-
-def asym_2(n1,n2,n3):
-    n_abs = math.sqrt(n1**2+n2**2+n3**2)
-    tmp = -1/4/math.pi/n_abs
-    tmp = tmp - (n1**4 + n2**4 + n3**4 - 3*n1**2*n2**2 - 3*n2**2*n3**2 -
-            3*n3**2*n1**2)/16/math.pi/n_abs**7
-    return tmp
-
-def asym_3(n1,n2,n3):
-    n_abs = math.sqrt(n1**2+n2**2+n3**2)
-    s1 = asym_2(n1,n2,n3)
-    s2 = 3*(   23 * (n1**8 + n2**8 + n3**8) 
-            - 244 * (n2**6 * n3**2 + n3**6 * n2**2 + 
-                     n1**6 * n2**2 + n2**6 * n1**2 + 
-                     n1**6 * n3**2 + n3**6 * n1**2  
-                     )
-            + 621 * (n1**4 * n2**4 + n2**4 * n3**4 + n3**4 * n1**4 )
-            - 228 * (n1**4 * n2**2 * n3**2 
-                     + n1**2 * n2**4 * n3**2
-                     + n1**2 * n2**2 * n3**4))
-
-    coef = 1/(768 * math.pi) * 8
-    s2 = -s2 / n_abs**13 /4 * coef
-    
-    return s1 + s2
-    
+#///////
+from lgf_asym import *
+from lgf_integrator import *
 
 
-NN = 1
-for n1 in range(1,NN):
-    for n2 in range(1,NN):
-        for n3 in range(1,NN):
-            print(n1, n2, n3)
-            
-            ft = lambda t : f(n1, n2, n3, t)
-            result,error = integrate.quad(ft, 0, np.inf, epsabs=1e-15)
+N_max    = 110
+n_sample = 100
 
-            a2 = asym_2(n1,n2,n3)
-            a3 = asym_3(n1,n2,n3)
-            print(result, a2, a3)
-            print(result + a2) 
-            print(result + a3) # definition has a sign difference
+n_abs   = [.0] * n_sample
+n_error_2 = [.0] * n_sample
+n_error_3 = [.0] * n_sample
+
+for i in range(0, n_sample):
+    n1 = random.randint(50, N_max)
+    n2 = random.randint(50, N_max)
+    n3 = random.randint(50, N_max)
+
+    print(n1, n2, n3)
+    a_int = lgf_int(n1,n2,n3)
+
+    a2 = asym_2(n1,n2,n3)
+    a3 = asym_3(n1,n2,n3)
+    print(a_int, a2, a3)
+
+    n_abs[i] = (n1**2 + n2**2 + n3**2)**(1/2)
+    n_error_2[i] = a_int + a2
+    n_error_3[i] = a_int + a3
+    print(abs(n_error_2[i])) # definition has a sign difference
+    print(abs(n_error_3[i])) # definition has a sign difference
 
 
+import matplotlib.pyplot as plt
+plt.plot(n_abs, n_error_2, 'bo', n_abs, n_error_3, 'ro')
+plt.yscale('log')
+plt.ylim(1e-18,1e-10)
+plt.show()
 
--- lgf/lgf_table_gen/python_lgf.py
+++ lgf/lgf_table_gen/python_lgf.py
@@ -1,37 +1,25 @@
+# Use Python3!!!!!!!!!!!!!!!!!!!! python 2 doesn't give right integral....
+
 import scipy.special as special
 import scipy.integrate as integrate
 import numpy as np
 import math
 
-f = lambda n1, n2, n3, t : special.ive(n1,2*t) * special.ive(n2,2*t) *special.ive(n3,2*t)
+from lgf_integrator import *
 
 N=101
 fname = open("lgf_table_100.hpp", "w")
 
-
 for n1 in range(0,N):
     for n2 in range(0, n1+1):
         for n3 in range(0,n2+1):
-            ft = lambda t : f(n1, n2, n3, t)
-            result,error = integrate.quad(ft, 0, np.inf, epsabs=1e-16,
-                    epsrel=1e-15)
-
-            if math.isnan(result): # ocasionally it has problem integrating. 
-                dn = 0.0
-                while math.isnan(result): # ocasionally it has problem integrating. 
-                    dn = dn + 1e-2
-
-                    ft1 = lambda t : f(n1, n2, n3+dn, t)
-                    ft2 = lambda t : f(n1, n2, n3-dn, t)
-                    result1,error = integrate.quad(ft1, 0, np.inf)
-                    result2,error = integrate.quad(ft2, 0, np.inf)
-                    result = (result1 + result2)/2.0
-
             print(n1, n2, n3)
-            print(result, error)
+
+            ft = lambda t : f(n1, n2, n3, t)
+            result= lgf_int(n1,n2,n3)
 
             fname.write("{:.20e},\n".format(result))
 
 
 fname.close()
-
+# Use Python3!!!!!!!!!!!!!!!!!!!! python 2 doesn't give right integral....

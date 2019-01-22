b86c508dc6b1efb8749e39bbfbebd366f043605c
Benedikt <benedikt.dorschner@googlemail.com>
2019-01-21 20:22:06 -0800

    add

---
+++ include/domain/decomposition/serverclient_traits.hpp
@@ -0,0 +1,41 @@
+#ifndef DOMAIN_INCLUDED_SERVERCLIENT_TRAITS_HPP
+#define DOMAIN_INCLUDED_SERVERCLIENT_TRAITS_HPP
+
+#include <vector>
+#include <stdexcept>
+#include <boost/mpi.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/mpi/status.hpp>
+
+#include <global.hpp>
+#include <domain/decomposition/compute_task.hpp>
+
+#include <domain/mpi/task_manager.hpp>
+#include <domain/mpi/query_registry.hpp>
+#include <domain/mpi/task.hpp>
+
+namespace domain
+{
+
+using namespace sr_mpi;
+template<class Domain>
+struct ServerClientTraits
+{
+
+    using domain_t = Domain;
+    using key_t  = typename  domain_t::key_t;
+    using key_query_t = Task<tags::key_query,std::vector<key_t>>;
+    using rank_query_t = Task<tags::key_query,std::vector<int>>;
+    using task_manager_t = TaskManager<key_query_t, rank_query_t>;
+};
+
+
+}
+
+
+
+
+
+
+#endif

6a6cddc3aa4d4f3bc3fb357e221a5af76fe8c845
WeiHou1996 <pathou@live.cn>
2021-10-06 14:39:48 -0700

    added Helmholtz LGF integration, haven't debugged

---
+++ include/iblgf/lgf/helmholtz.hpp
@@ -0,0 +1,96 @@
+//      ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄
+//     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+//      ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀
+//          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌
+//          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄
+//          ▐░▌     ▐░░░░░░░░░░▌ ▐░▌          ▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌
+//          ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░▌ ▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀
+//          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌
+//      ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌
+//     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌
+//      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀
+
+#ifndef INCLUDED_HELMHOLTZ_HPP
+#define INCLUDED_HELMHOLTZ_HPP
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <complex>
+#include <boost/math/special_functions/bessel.hpp>
+#include <boost/align/aligned_allocator_adaptor.hpp>
+
+#include <iblgf/global.hpp>
+#include <iblgf/domain/dataFields/dataBlock.hpp>
+#include <iblgf/domain/dataFields/datafield.hpp>
+#include <iblgf/lgf/helmholtz_lookup.hpp>
+#include <iblgf/lgf/lgf.hpp>
+
+namespace iblgf
+{
+namespace lgf
+{
+using namespace domain;
+
+template<std::size_t Dim>
+class Helmholtz : public LGF_Base<Dim, LGF_GL<Dim>>
+{
+  public: //Ctor:
+    using super_type = LGF_Base<Dim, LGF_GL<Dim>>;
+    using block_descriptor_t = BlockDescriptor<int, Dim>;
+    using coordinate_t = typename block_descriptor_t::coordinate_type;
+    using complex_vector_t = typename super_type::complex_vector_t;
+
+    using key_3D = std::tuple<int, int, int>;
+    using key_2D = std::tuple<int, int>;
+    using level_map_3D_t = std::map<key_3D, std::unique_ptr<complex_vector_t>>;
+    using level_map_2D_t = std::map<key_2D, std::unique_ptr<complex_vector_t>>;
+
+  public: //Ctor:
+    Helmholtz(float_type C)
+    : dft_level_maps_3D(super_type::max_lgf_map_level)
+    , dft_level_maps_2D(super_type::max_lgf_map_level)
+    , c(C)
+    {
+        origin = Helmholtz_Lookup::origin_val(c);
+    }
+    template<int Dim1 = Dim>
+    auto get_key(const block_descriptor_t& _b, typename std::enable_if<Dim1 == 3, int>::type _level_diff) const noexcept
+    {
+        const auto base = _b.base();
+        return key_3D(base[0], base[1], base[2]);
+    }
+
+
+    template<int Dim1 = Dim>
+    auto get_key(const block_descriptor_t& _b, typename std::enable_if<Dim1 == 2, int>::type _level_diff) const noexcept
+    {
+        const auto base = _b.base();
+        return key_2D(base[0], base[1]);
+    }
+    void build_lt() {}
+    void change_level_impl(int _level_diff) {
+	//if (Dim == 2) c_diff = -static_cast<float_type>(_level_diff)/2.0/M_PI*std::log(2.0);
+    }
+
+    auto get(const coordinate_t& _c) const noexcept
+    {
+        if (Dim == 2) return Helmholtz_Lookup::get<coordinate_t>(_c) - origin;
+        else {return Helmholtz_Lookup::get<coordinate_t>(_c) - origin;}
+
+    }
+
+  public:
+    std::vector<level_map_3D_t> dft_level_maps_3D; ///<lgf map for octants per level
+    std::vector<level_map_2D_t> dft_level_maps_2D; ///<lgf map for octants per level
+  private:
+    float_type c_diff = 0.0;
+    float_type origin = 0.0;
+    float_type c = 0.0;
+};
+
+} // namespace lgf
+} // namespace iblgf
+
+#endif
---
+++ include/iblgf/lgf/helmholtz_lookup.hpp
@@ -0,0 +1,161 @@
+//      ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄
+//     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+//      ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀
+//          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌
+//          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄
+//          ▐░▌     ▐░░░░░░░░░░▌ ▐░▌          ▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌
+//          ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░▌ ▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀
+//          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌
+//      ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌
+//     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌
+//      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀
+
+#ifndef INCLUDED_HELMHOLTZ_LOOKUP_HPP
+#define INCLUDED_HELMHOLTZ_LOOKUP_HPP
+
+#include <algorithm>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <cmath>
+#include <boost/math/special_functions/hypergeometric_pFq.hpp>
+
+#include <iblgf/global.hpp>
+
+namespace iblgf
+{
+namespace lgf
+{
+class Helmholtz_Lookup
+{
+  public:
+    template<class Coordinate>
+    static typename std::enable_if<Coordinate::size() == 3, float_type>::type get(const Coordinate& _c) noexcept
+    {
+        int x = std::abs(static_cast<int>(_c.x()));
+        int y = std::abs(static_cast<int>(_c.y()));
+        int z = std::abs(static_cast<int>(_c.z()));
+
+        if (x <= N_max_3D && y <= N_max_3D && z <= N_max_3D) // using table
+        {
+            if (x < y) std::swap(x, y);
+            if (x < z) std::swap(x, z);
+            if (y < z) std::swap(y, z);
+
+            return -table_3D[(x * (2 + 3 * x + x * x)) / 6 + y * (y + 1) / 2 +
+                           z]; // indexing
+        }
+        else
+        {
+            return asym(x, y, z);
+        }
+    }
+
+
+
+    template<class Coordinate>
+    static typename std::enable_if<Coordinate::size() == 2, float_type>::type get(const Coordinate& _c) noexcept
+    {
+	int x = std::abs(static_cast<int>(_c.x()));
+	int y = std::abs(static_cast<int>(_c.y()));
+
+	if (x <= N_max_2D && y <= N_max_2D) // using table
+	{
+		if (x < y) std::swap(x, y);
+		return -table_2D[x*(N_max_2D+2)+y]; 
+	}
+	else
+	{
+		return -asym(x, y);
+	}
+    }
+
+    class integrand {
+        public:
+        integrand(const float_type C, int N, int M) : c(c) {
+            n = std::abs(N);
+            m = std::abs(m);
+        }
+        float_type operator()(const float_type& x) const {
+            float_type a = 4.0 - 2.0*std::cos(x)+c*c;
+            float_type K = (a + std::sqrt((a*a - 4.0))/2.0;
+            float_type int_val = (1.0 - std::cos(x*m))/(std::pow(K,n))*(1/(K - 1.0/K));
+            return int_val/2.0/M_PI;
+        }
+
+        private:
+        int n;
+        int m;
+        float_type c;
+    };
+
+    static float_type asym(int n1, int n2) {
+	    float_type Cfund = -0.18124942796; // constant for asymptotic expansion (C_fund)
+	    float_type Integral_val = -0.076093998454228; // constant for asymptotical expansion (integral)	
+	    float_type r = std::sqrt(n1*n1 + n2*n2);
+
+	    float_type theta = atan2(n2,n1);
+	    float_type second_term = 1.0/24.0/M_PI*cos(4.0*theta)/r/r;
+	    float_type res = -0.5/M_PI*log(r) + Cfund + Integral_val + second_term;
+	    return res;
+    }
+
+    static float_type origin_val(float_type c) {
+        float_type z = 2.0/(2.0+c*c/2.0) * 2.0/(2.0+c*c/2.0);
+        return 0.5/(2.0+c*c/2.0)*boost::math::hypergeometric_pFq({0.5, 0.5},{1}, z);
+    }
+    template<typename function_type>
+    static float_type integral(const float_type a, const float_type b,
+        const float_type tol, function_type func)
+    {
+        unsigned n = 1U;
+
+        float_type h = (b - a);
+        float_type I = (func(a) + func(b)) * (h / 2);
+
+        for (unsigned k = 0U; k < 8U; k++)
+        {
+            h /= 2;
+
+            float_type sum(0);
+            for (unsigned j = 1U; j <= n; j++)
+            {
+                sum += func(a + (float_type((j * 2) - 1) * h));
+            }
+
+            const float_type I0 = I;
+            I = (I / 2) + (h * sum);
+
+            const float_type ratio = I0 / I;
+            const float_type delta = ratio - 1;
+            const float_type delta_abs = ((delta < 0) ? -delta : delta);
+
+            if ((k > 1U) && (delta_abs < tol)) { break; }
+
+            n *= 2U;
+        }
+
+        return I;
+    }
+
+    //TODO: Vectorize this function. Takes a lot of time!
+    static float_type integralHelmholtz(int n1, int n2, float_type c) {
+        return integral(-M_PI, M_PI, 1.0e-14, integrand(c, n1, n2));
+    }
+
+  private:
+    static const int                     N_max_3D;
+    static const std::vector<float_type> table_3D;
+
+    
+    static const int                     N_max_2D;
+    static const std::vector<float_type> table_2D;
+
+};
+
+//decltype(Lookup::N_max) Lookup::N_max(100);
+
+} // namespace lgf
+} // namespace iblgf
+#endif

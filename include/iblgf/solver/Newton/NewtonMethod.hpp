65c7e18469958f96bd83dfebaff3fc81f42c2972
WeiHou1996 <pathou@live.cn>
2022-02-05 23:07:06 -0800

    misc

--- include/iblgf/solver/Newton/NewtonMethod.hpp
+++ include/iblgf/solver/Newton/NewtonMethod.hpp
@@ -1504,7 +1504,7 @@ class NewtonIteration
     }
 
     template<class Source, class Target>
-    void Curl() noexcept
+    void curl() noexcept
     {
         auto client = domain_->decomposition().client();
 

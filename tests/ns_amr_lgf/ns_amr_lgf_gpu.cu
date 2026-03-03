8cd3030d8b392706016cc546e2bd8d020fab2d19
Caroline Cardinale <ccardina@caltech.edu>
2026-03-03 14:33:47 -0600

    Add GPU tests for ns and poisson

---
+++ tests/ns_amr_lgf/ns_amr_lgf_gpu.cu
@@ -0,0 +1,54 @@
+//      ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄
+//     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+//      ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀
+//          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌
+//          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄
+//          ▐░▌     ▐░░░░░░░░░░▌ ▐░▌          ▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌
+//          ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░▌ ▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀
+//          ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌
+//      ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌
+//     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌
+//      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀
+#ifndef IBLGF_COMPILE_CUDA
+#define IBLGF_COMPILE_CUDA
+#endif
+#include <boost/mpi.hpp>
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
+
+#include "ns_amr_lgf.hpp"
+#include <iblgf/dictionary/dictionary.hpp>
+
+
+using namespace iblgf;
+
+int main(int argc, char *argv[])
+{
+
+	boost::mpi::environment env(argc, argv);
+	boost::mpi::communicator world;
+
+	std::string input="./";
+    input += std::string("configFile");
+
+    if (argc>1 && argv[1][0] != '-')
+    {
+        input = argv[1];
+    }
+    int rank = world.rank();
+    // get number of GPUs and set device
+    int deviceCount = 0;
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+    std::cout<<"Rank "<<rank<<" found "<<deviceCount<<" GPUs"<<std::endl;
+    cudaSetDevice(rank % deviceCount);
+    // Read in dictionary
+    Dictionary dictionary(input, argc, argv);
+
+    //Instantiate setup
+    NS_AMR_LGF setup(&dictionary);
+
+    // run setup
+    setup.run();
+
+    return 0;
+}

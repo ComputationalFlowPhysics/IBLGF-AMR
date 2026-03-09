f4183ae81aeae36a84dec70c567f2dde21cfa0e3
Caroline Cardinale <ccardina@caltech.edu>
2026-03-09 12:52:47 -0700

    Add OpenMP parallelization to domain operations in LinSysSolver

--- include/iblgf/solver/linsys/linsys.hpp
+++ include/iblgf/solver/linsys/linsys.hpp
@@ -96,8 +96,8 @@ class LinSysSolver
         //    printvec(tmp, "u");
         //}
 
-        domain::Operator::domainClean<face_aux_type>(domain_);
-        domain::Operator::add_field_expression<face_aux_type>(domain_, simulation_->frame_vel(), 1.0);
+        this->template domainClean_omp<face_aux_type>();
+        this->template add_field_expression_omp<face_aux_type>(simulation_->frame_vel(), 1.0);
 
 	real_coordinate_type tmp_coord(0.0);
 
@@ -131,24 +131,24 @@ class LinSysSolver
         this->subtract_boundary_vel(uc, t);
 
         domain_->client_communicator().barrier();
-        domain::Operator::domainClean<face_aux2_type>(domain_);
+        this->template domainClean_omp<face_aux2_type>();
         this->template CG_solve<face_aux2_type>(uc, alpha);
     }
 
     template<class Field>
     void pressure_correction()
     {
-        domain::Operator::domainClean<face_aux2_type>(domain_);
-        domain::Operator::domainClean<cell_aux2_type>(domain_);
+        this->template domainClean_omp<face_aux2_type>();
+        this->template domainClean_omp<cell_aux2_type>();
         this->smearing<face_aux2_type>(ib_->force());
 
         int l = domain_->tree()->depth()-1;
-        domain::Operator::levelDivergence<face_aux2_type, cell_aux2_type>(domain_, l);
+        this->template levelDivergence_omp<face_aux2_type, cell_aux2_type>(l);
 
         // apply L^-1
         psolver_.template apply_lgf<cell_aux2_type,cell_aux2_type>(MASK_TYPE::IB2AMR);
 
-        domain::Operator::add<cell_aux2_type, Field>(domain_, -1.0);
+        this->template domainAdd_omp<cell_aux2_type, Field>(-1.0);
     }
 
     template<class ForceType>
@@ -268,10 +268,8 @@ class LinSysSolver
     template <class Field, class VecType>
     void ET_H_S_E(VecType& fin, VecType& fout, float_type alpha)
     {
-        auto client = domain_->decomposition().client();
-
-        domain::Operator::domainClean<Field>(domain_);
-        domain::Operator::domainClean<face_aux_type>(domain_);
+        this->template domainClean_omp<Field>();
+        this->template domainClean_omp<face_aux_type>();
 
         this->smearing<Field>(fin);
 
@@ -287,7 +285,7 @@ class LinSysSolver
 
         this->template apply_Schur<Field, face_aux_type>(MASK_TYPE::xIB2IB);
 
-        domain::Operator::add<face_aux_type, Field>(domain_, -1.0);
+        this->template domainAdd_omp<face_aux_type, Field>(-1.0);
 
         //this->template apply_Schur_withIF<Field, face_aux_type>(MASK_TYPE::xIB2IB, alpha);
         //if (std::fabs(alpha)>1e-14)
@@ -309,7 +307,7 @@ class LinSysSolver
 
         // div
         this->template levelDivergence_omp<Source, cell_aux2_type>(l);
-        domain::Operator::clean_ib_region_boundary<cell_aux2_type>(domain_, l);
+        this->template clean_ib_region_boundary_omp<cell_aux2_type>(l);
 
         // apply L^-1
         psolver_.template apply_lgf<cell_aux2_type, cell_aux2_type>(fmm_type);
@@ -323,7 +321,8 @@ class LinSysSolver
 
         for (int l = l_min; l < l_max; ++l) {
             this->template levelGradient_omp<cell_aux2_type, Target>(l);
-            if (fmm_type ==  MASK_TYPE::xIB2IB) domain::Operator::clean_ib_region_boundary<Target>(domain_, l);
+            if (fmm_type ==  MASK_TYPE::xIB2IB)
+                this->template clean_ib_region_boundary_omp<Target>(l);
         }
     }
 
@@ -366,7 +365,7 @@ class LinSysSolver
 
         //cleaning
         if (cleaning)
-            domain::Operator::domainClean<U>(domain_);
+            this->template domainClean_omp<U>();
 
         for (std::size_t i=0; i<ib_->size(); ++i)
         {
@@ -403,6 +402,281 @@ class LinSysSolver
         return octants;
     }
 
+    std::vector<octant_t*> collect_domain_octants(
+        bool local_only = false,
+        bool require_data = true) const
+    {
+        std::vector<octant_t*> octants;
+        octants.reserve(domain_->num_allocations());
+        for (auto it = domain_->begin(); it != domain_->end(); ++it)
+        {
+            auto* oct = it.ptr();
+            if (!oct) continue;
+            if (local_only && !oct->locally_owned()) continue;
+            if (require_data && !oct->has_data()) continue;
+            if (require_data && !oct->data().is_allocated()) continue;
+            octants.push_back(oct);
+        }
+        return octants;
+    }
+
+    template<typename Field>
+    void domainClean_omp() noexcept
+    {
+        auto octants = collect_domain_octants(false, true);
+        IBLGF_LINSYS_OMP_PARALLEL_FOR
+        for (std::size_t oct_idx = 0; oct_idx < octants.size(); ++oct_idx)
+        {
+            auto* oct = octants[oct_idx];
+            if (!oct) continue;
+            for (std::size_t field_idx = 0; field_idx < Field::nFields();
+                 ++field_idx)
+            {
+                auto& lin_data =
+                    oct->data_r(Field::tag(), field_idx).linalg_data();
+                std::fill(lin_data.begin(), lin_data.end(), 0.0);
+            }
+        }
+    }
+
+    template<typename Field, typename Func>
+    void add_field_expression_omp(
+        Func& f,
+        float_type t,
+        float_type scale = 1.0) noexcept
+    {
+        const auto dx_base = domain_->dx_base();
+        auto octants = collect_domain_octants(true, true);
+        IBLGF_LINSYS_OMP_PARALLEL_FOR
+        for (std::size_t oct_idx = 0; oct_idx < octants.size(); ++oct_idx)
+        {
+            auto* oct = octants[oct_idx];
+            if (!oct) continue;
+
+            for (std::size_t field_idx = 0; field_idx < Field::nFields();
+                 ++field_idx)
+            {
+                for (auto& n : oct->data().node_field())
+                {
+                    auto coord = n.global_coordinate() * dx_base;
+                    n(Field::tag(), field_idx) +=
+                        f(field_idx, t, coord) * scale;
+                }
+            }
+        }
+    }
+
+    template<typename From, typename To>
+    void domainAdd_omp(float_type scale = 1.0) noexcept
+    {
+        static_assert(From::nFields() == To::nFields(),
+            "number of fields doesn't match when add");
+
+        auto octants = collect_domain_octants(true, true);
+        IBLGF_LINSYS_OMP_PARALLEL_FOR
+        for (std::size_t oct_idx = 0; oct_idx < octants.size(); ++oct_idx)
+        {
+            auto* oct = octants[oct_idx];
+            if (!oct) continue;
+
+            for (std::size_t field_idx = 0; field_idx < From::nFields();
+                 ++field_idx)
+            {
+                for (auto& n : oct->data().node_field())
+                    n(To::tag(), field_idx) +=
+                        n(From::tag(), field_idx) * scale;
+            }
+        }
+    }
+
+    template<typename Field>
+    void clean_ib_region_boundary_omp(
+        int l,
+        int clean_width = 2) noexcept
+    {
+        auto octants = collect_level_octants(l, true, true);
+        const int dim = domain_->dimension();
+
+        IBLGF_LINSYS_OMP_PARALLEL_FOR
+        for (std::size_t oct_idx = 0; oct_idx < octants.size(); ++oct_idx)
+        {
+            auto* oct = octants[oct_idx];
+            if (!oct) continue;
+
+            for (std::size_t i = 0; i < oct->num_neighbors(); ++i)
+            {
+                auto it2 = oct->neighbor(i);
+                if ((!it2 || !it2->has_data()) || (!it2->is_ib()))
+                {
+                    for (std::size_t field_idx = 0; field_idx < Field::nFields();
+                         ++field_idx)
+                    {
+                        auto& lin_data =
+                            oct->data_r(Field::tag(), field_idx).linalg_data();
+                        const int N = oct->data().descriptor().extent()[0];
+
+                        if (dim == 3)
+                        {
+                            if (i == 4)
+                                view(lin_data, xt::all(), xt::all(),
+                                    xt::range(0, clean_width)) *= 0.0;
+                            else if (i == 10)
+                                view(lin_data, xt::all(),
+                                    xt::range(0, clean_width), xt::all()) *=
+                                    0.0;
+                            else if (i == 12)
+                                view(lin_data, xt::range(0, clean_width),
+                                    xt::all(), xt::all()) *= 0.0;
+                            else if (i == 14)
+                                view(lin_data,
+                                    xt::range(N + 2 - clean_width, N + 3),
+                                    xt::all(), xt::all()) *= 0.0;
+                            else if (i == 16)
+                                view(lin_data, xt::all(),
+                                    xt::range(N + 2 - clean_width, N + 3),
+                                    xt::all()) *= 0.0;
+                            else if (i == 22)
+                                view(lin_data, xt::all(), xt::all(),
+                                    xt::range(N + 2 - clean_width, N + 3)) *=
+                                    0.0;
+                        }
+                        if (dim == 2)
+                        {
+                            if (i == 1)
+                                view(lin_data, xt::all(),
+                                    xt::range(0, clean_width)) *= 0.0;
+                            else if (i == 3)
+                                view(lin_data, xt::range(0, clean_width),
+                                    xt::all()) *= 0.0;
+                            else if (i == 5)
+                                view(lin_data,
+                                    xt::range(N + 2 - clean_width, N + 3),
+                                    xt::all()) *= 0.0;
+                            else if (i == 7)
+                                view(lin_data, xt::all(),
+                                    xt::range(N + 2 - clean_width, N + 3)) *=
+                                    0.0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    template<typename Field>
+    void clean_leaf_correction_boundary_omp(
+        int l,
+        bool leaf_only_boundary = false,
+        int clean_width = 1) noexcept
+    {
+        auto octants = collect_level_octants(l, false, false);
+
+        IBLGF_LINSYS_OMP_PARALLEL_FOR
+        for (std::size_t oct_idx = 0; oct_idx < octants.size(); ++oct_idx)
+        {
+            auto* oct = octants[oct_idx];
+            if (!oct || oct->locally_owned()) continue;
+            if (!oct->has_data() || !oct->data().is_allocated()) continue;
+
+            for (std::size_t field_idx = 0; field_idx < Field::nFields();
+                 ++field_idx)
+            {
+                auto& lin_data =
+                    oct->data_r(Field::tag(), field_idx).linalg_data();
+                std::fill(lin_data.begin(), lin_data.end(), 0.0);
+            }
+        }
+
+        IBLGF_LINSYS_OMP_PARALLEL_FOR
+        for (std::size_t oct_idx = 0; oct_idx < octants.size(); ++oct_idx)
+        {
+            auto* oct = octants[oct_idx];
+            if (!oct || !oct->locally_owned()) continue;
+            if (!oct->has_data() || !oct->data().is_allocated()) continue;
+            if (!leaf_only_boundary) continue;
+            if (!(oct->is_correction() || oct->is_old_correction())) continue;
+
+            for (std::size_t field_idx = 0; field_idx < Field::nFields();
+                 ++field_idx)
+            {
+                auto& lin_data =
+                    oct->data_r(Field::tag(), field_idx).linalg_data();
+                std::fill(lin_data.begin(), lin_data.end(), 0.0);
+            }
+        }
+
+        if (l != domain_->tree()->base_level()) return;
+
+        auto local_octants = collect_level_octants(l, true, true);
+        const int dim = domain_->dimension();
+        IBLGF_LINSYS_OMP_PARALLEL_FOR
+        for (std::size_t oct_idx = 0; oct_idx < local_octants.size(); ++oct_idx)
+        {
+            auto* oct = local_octants[oct_idx];
+            if (!oct) continue;
+
+            for (std::size_t i = 0; i < oct->num_neighbors(); ++i)
+            {
+                auto it2 = oct->neighbor(i);
+                if ((!it2 || !it2->has_data()) ||
+                    (leaf_only_boundary &&
+                        (it2->is_correction() || it2->is_old_correction())))
+                {
+                    for (std::size_t field_idx = 0; field_idx < Field::nFields();
+                         ++field_idx)
+                    {
+                        auto& lin_data =
+                            oct->data_r(Field::tag(), field_idx).linalg_data();
+                        const int N = oct->data().descriptor().extent()[0];
+
+                        if (dim == 3)
+                        {
+                            if (i == 4)
+                                view(lin_data, xt::all(), xt::all(),
+                                    xt::range(0, clean_width)) *= 0.0;
+                            else if (i == 10)
+                                view(lin_data, xt::all(),
+                                    xt::range(0, clean_width), xt::all()) *=
+                                    0.0;
+                            else if (i == 12)
+                                view(lin_data, xt::range(0, clean_width),
+                                    xt::all(), xt::all()) *= 0.0;
+                            else if (i == 14)
+                                view(lin_data,
+                                    xt::range(N + 2 - clean_width, N + 3),
+                                    xt::all(), xt::all()) *= 0.0;
+                            else if (i == 16)
+                                view(lin_data, xt::all(),
+                                    xt::range(N + 2 - clean_width, N + 3),
+                                    xt::all()) *= 0.0;
+                            else if (i == 22)
+                                view(lin_data, xt::all(), xt::all(),
+                                    xt::range(N + 2 - clean_width, N + 3)) *=
+                                    0.0;
+                        }
+                        else if (dim == 2)
+                        {
+                            if (i == 1)
+                                view(lin_data, xt::all(),
+                                    xt::range(0, clean_width)) *= 0.0;
+                            else if (i == 3)
+                                view(lin_data, xt::range(0, clean_width),
+                                    xt::all()) *= 0.0;
+                            else if (i == 5)
+                                view(lin_data,
+                                    xt::range(N + 2 - clean_width, N + 3),
+                                    xt::all()) *= 0.0;
+                            else if (i == 7)
+                                view(lin_data, xt::all(),
+                                    xt::range(N + 2 - clean_width, N + 3)) *=
+                                    0.0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     template<class Source, class Target>
     void levelDivergence_omp(int l) noexcept
     {
@@ -420,7 +694,7 @@ class LinSysSolver
             domain::Operator::divergence<Source, Target>(oct->data(), dx_level);
         }
 
-        domain::Operator::clean_leaf_correction_boundary<Target>(domain_, l, true, 2);
+        this->template clean_leaf_correction_boundary_omp<Target>(l, true, 2);
     }
 
     template<class Source, class Target>
@@ -467,7 +741,7 @@ class LinSysSolver
 
         //cleaning
         if (cleaning)
-            domain::Operator::domainClean<U>(domain_);
+            this->template domainClean_omp<U>();
 
         for (std::size_t i=0; i<ib_->size(); ++i)
         {
